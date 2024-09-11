05ba23e6ccbf5f2f987285e3cd5e017ffbfc45e5
honeybeefe <44984498a@gmail.com>
2024-09-11 19:47:10 +0900

    2024.09.11 캐릭터 이동 시스템

--- Endless Runner/Assets/Scripts/Runner.cs
+++ Endless Runner/Assets/Scripts/Runner.cs
@@ -12,14 +12,56 @@ public enum RoadLine
 public class Runner : MonoBehaviour
 {
     [SerializeField] RoadLine roadLine;
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] float speed = 25.0f;
+    [SerializeField] float positionX = 2.5f; 
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
     void Start()
     {
-        
+        roadLine = RoadLine.MIDDLE;
+    }
+
+    void OnKeyUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(roadLine!=RoadLine.LEFT)
+            {
+                roadLine --;
+
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(roadLine!=RoadLine.RIGHT)
+            {
+                roadLine++;
+            }
+        }
     }
 
     void Update()
     {
-        
+        OnKeyUpdate();
+    }
+
+    private void Move()
+    {
+        rigidbody.position= Vector3.Lerp
+         (
+            rigidbody.position,
+            new Vector3((int)roadLine * positionX, 0, 0),
+            speed*Time.fixedDeltaTime
+            );
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 }
